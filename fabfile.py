80a01e6147554ce9f0321c99385d5b3e160f7acd
Danilo Bargen <gezuru@gmail.com>
2012-07-20 12:08:02 +0200

    Sitemap generator, fabfile

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import local, env
+from fabric.contrib.project import rsync_project
+
+env.hosts = ['ich-wars-nicht.ch']
+env.path = '/var/www/dbrgn/blog/'
+
+def build():
+    local('rm -rf _build/ && run-rstblog build')
+    local('python gensitemap.py > _build/sitemap.xml')
+
+def sync():
+    rsync_project(remote_dir=env.path,
+                  local_dir='_build/',
+                  delete=True,
+                  exclude=['*.py', '*.pyc', 'requirements.txt'])
+
+def deploy():
+    build()
+    sync()
