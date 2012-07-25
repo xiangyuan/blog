cf6eabf1de7e0dc621de3a6cd0cc6954626fc8d0
Danilo Bargen <gezuru@gmail.com>
2012-07-25 02:10:59 +0200

    fabfile: add timestamp to css files

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+import time
 from fabric.api import local, env
 from fabric.contrib.project import rsync_project
 
@@ -13,13 +14,19 @@ def serve():
 def build():
     # Build HTML
     local('rm -rf _build/ && run-rstblog build')
+
     # Generate sitemaps
     local('python gensitemap.py > _build/sitemap.xml')
+
     # Minify CSS
     local('cssmin < _build/static/style.css > _build/static/style.min.css')
     local('mv _build/static/style.min.css _build/static/style.css')
     local('cssmin < _build/static/_pygments.css > _build/static/_pygments.min.css')
     local('mv _build/static/_pygments.min.css _build/static/_pygments.css')
+
+    # Add timestamp to css files
+    local('find _build -type f -exec sed -i "s/style.css/style.css?%s/g" {} \;' % int(time.time()))
+    local('find _build -type f -exec sed -i "s/_pygments.css/_pygments.css?%s/g" {} \;' % int(time.time()))
 
 def sync():
     rsync_project(remote_dir=env.path,
