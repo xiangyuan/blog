4a65b6cce40316aaf0c35aacbcd99dbab7164e6f
Danilo Bargen <gezuru@gmail.com>
2013-08-31 00:45:30 +0200

    Fix in merge.sh

--- serve.sh
+++ serve.sh
@@ -8,7 +8,7 @@ INPUTDIR=$BASEDIR/content
 OUTPUTDIR=$BASEDIR/output
 CONFFILE=$BASEDIR/pelicanconf.py
 
-rm -r output \
+rm -rf output \
 && pelican --verbose $INPUTDIR -o $OUTPUTDIR -s $CONFFILE $PELICANOPTS \
 && cd output \
 && python -m SimpleHTTPServer
